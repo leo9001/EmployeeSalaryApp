89fd66e471b432ea375a2741c28fcfbc812507bc
saifur 1000 <leo9001@gmail.com>
2014-11-06 17:24:57 +0600

    First Commit
    This is my first commit

---
+++ EmployeeSalaryApp/EmployeeSalaryUI.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace EmployeeSalaryApp
+{
+    partial class employeeSalaryUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.employeeListBox = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.resultListView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Id :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salary Amount :";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(98, 6);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(251, 20);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(98, 32);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(251, 20);
+            this.idTextBox.TabIndex = 4;
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(98, 58);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(251, 20);
+            this.amountTextBox.TabIndex = 5;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(274, 84);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(274, 113);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 7;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // employeeListBox
+            // 
+            this.employeeListBox.FormattingEnabled = true;
+            this.employeeListBox.Location = new System.Drawing.Point(98, 142);
+            this.employeeListBox.Name = "employeeListBox";
+            this.employeeListBox.Size = new System.Drawing.Size(251, 82);
+            this.employeeListBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(167, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Total Amount :";
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.Location = new System.Drawing.Point(249, 230);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.totalTextBox.TabIndex = 10;
+            // 
+            // resultListView
+            // 
+            this.resultListView.GridLines = true;
+            this.resultListView.Location = new System.Drawing.Point(14, 267);
+            this.resultListView.Name = "resultListView";
+            this.resultListView.Size = new System.Drawing.Size(335, 119);
+            this.resultListView.TabIndex = 11;
+            this.resultListView.UseCompatibleStateImageBehavior = false;
+            this.resultListView.View = System.Windows.Forms.View.Details;
+            // 
+            // employeeSalaryUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 404);
+            this.Controls.Add(this.resultListView);
+            this.Controls.Add(this.totalTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.employeeListBox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "employeeSalaryUI";
+            this.Text = "Employee Salary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListBox employeeListBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.ListView resultListView;
+    }
+}
+
---
+++ EmployeeSalaryApp/EmployeeSalaryUI.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeSalaryApp
+{
+    public partial class employeeSalaryUI : Form
+    {
+        public employeeSalaryUI()
+        {
+            InitializeComponent();
+        }
+
+        private string fileLocation = @"employee.txt";
+
+        private void saveButton_Click(object sender, EventArgs e)
+
+        {
+            string name = nameTextBox.Text;
+            string id = idTextBox.Text;
+            string amount = amountTextBox.Text;
+
+            FileStream aFileStream = new FileStream(fileLocation, FileMode.Append);
+
+            StreamWriter aStreamWriter = new StreamWriter(aFileStream);
+
+            aStreamWriter.WriteLine(name + ", " + id + ", " + amount);
+
+            aStreamWriter.Close();
+
+            nameTextBox.Text = "";
+            idTextBox.Text = "";
+            amountTextBox.Text = "";
+
+            nameTextBox.Focus();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            resultListView.Columns.Add("Name", 100);
+            resultListView.Columns.Add("Id", 100);
+            resultListView.Columns.Add("Salary", 100);
+
+            int totalSalary = 0;
+
+            FileStream aFileStream = new FileStream(fileLocation, FileMode.Open);
+
+            StreamReader aStreamReader = new StreamReader(aFileStream);
+
+            employeeListBox.Items.Clear();
+
+            while (!aStreamReader.EndOfStream)
+            {
+                string employeeInfo = aStreamReader.ReadLine();
+
+                employeeListBox.Items.Add(employeeInfo);
+
+                
+
+                string[] employeeDetails = employeeInfo.Split(',');
+
+                int salary = Convert.ToInt32(employeeDetails[2]);
+
+                // resultListView
+
+                string[] employeeData = {employeeDetails[0], employeeDetails[1], employeeDetails[2]};
+                ListViewItem lvi = new ListViewItem(employeeData);
+                resultListView.Items.Add(lvi);
+
+                totalSalary += salary;
+            }
+
+            aStreamReader.Close();
+
+            totalTextBox.Text = totalSalary.ToString();
+        }
+
+    }
+}
